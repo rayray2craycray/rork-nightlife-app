cd58c49553aff84dbbd505eec6c91bf5a4099e97
Rayan Taimur <rayan@Rayans-Air.hsd1.md.comcast.net>
2026-01-25 15:15:05 -0500

    Fix bcrypt import - use bcryptjs for compatibility

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,7 @@
  */
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   // Email/Password Authentication
